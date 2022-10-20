3e1772f88024dd25b44c3f9646dab0af68322274
zunohan <pnlan1406@gmail.com>
2022-10-20 20:19:19 +0700

    haha

--- src/init/db.ts
+++ src/init/db.ts
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
 const initDatabase = async () => {
-  const uri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
+  const uri = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
   try {
-    console.log({ uri, dbName: process.env.MONGO_DATABASE_NAME });
-    await mongoose.connect(uri, { dbName: process.env.MONGO_DATABASE_NAME });
+    await mongoose.connect(uri, {
+      auth: {
+        username: process.env.MONGO_USERNAME,
+        password: process.env.MONGO_PASSWORD,
+      },
+      authSource: "admin",
+      dbName: process.env.MONGO_DATABASE_NAME,
+    });
     console.log(
       "⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️ Connect to MONGODB inside docker ⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️"
     );
