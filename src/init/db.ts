844db71bccac300ab453506a5e01fdded574eef6
zunohan <pnlan1406@gmail.com>
2022-10-20 20:23:52 +0700

    haha

--- src/init/db.ts
+++ src/init/db.ts
@@ -4,11 +4,9 @@ const initDatabase = async () => {
   const uri = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
   try {
     await mongoose.connect(uri, {
-      auth: {
-        username: process.env.MONGO_USERNAME,
-        password: process.env.MONGO_PASSWORD,
-      },
       authSource: "admin",
+      user: process.env.MONGO_USERNAME,
+      pass: process.env.MONGO_PASSWORD,
       dbName: process.env.MONGO_DATABASE_NAME,
     });
     console.log(
