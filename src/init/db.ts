3a17273d22372584dad4eb0d4eaaf96d955ac009
zunohan <pnlan1406@gmail.com>
2023-02-03 13:44:25 +0700

    add mongourl

--- src/init/db.ts
+++ src/init/db.ts
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const initDatabase = async () => {
+  mongoose.set('strictQuery', true);
   try {
     await mongoose.connect(
       `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`,
-      { dbName: process.env.MONGO_DATABASE_NAME }
+      { dbName: process.env.MONGO_DATABASE_NAME },
     );
-    console.log("⚡️⚡️ Connect to MONGODB inside docker ⚡️⚡️");
+    console.log('⚡️⚡️ Connect to MONGODB inside docker ⚡️⚡️');
   } catch (error) {
     console.error(error);
-    // process.exit(1);
   }
 };
 
