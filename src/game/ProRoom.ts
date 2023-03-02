08ea4834580dbeba2304d9d039167af3035b97c4
zunohan <pnlan1406@gmail.com>
2023-03-02 23:47:12 +0700

    test pro room

--- src/game/ProRoom.ts
+++ src/game/ProRoom.ts
@@ -240,7 +240,7 @@ export default class ProRoom extends Room<RoomState> {
 
       if (chips >= player.chips) return this.allinAction(client.sessionId, player, player.chips); // trường hợp này chuyển sang allin
       // if (this.currentBet > chips + player.accumulatedBet + this.MIN_BET) return; // chỉ cho phép raise lệnh cao hơn current bet + min bet
-      if (chips < this.MIN_CHIP) return;
+      if (chips < this.MIN_BET) return;
       this.raiseAction(player, chips);
 
       this.sendNewState();
