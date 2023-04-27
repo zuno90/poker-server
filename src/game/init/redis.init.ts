27214a53e6bf5dd144b543145b14fbf44d239309
Tran Anh Pha <trananhpha2001@gmail.com>
2023-04-28 05:08:02 +0700

    import file redis init

---
+++ src/game/init/redis.init.ts
@@ -0,0 +1,22 @@
+import Redis from 'ioredis';
+
+const redisUri =
+  process.env.NODE_ENV === 'production' ? `${process.env.REDIS_URL}` : `localhost:6379`;
+
+console.log(redisUri);
+
+
+const redisMaster = new Redis(redisUri);
+const redisPub = new Redis(redisUri);
+const redisSub = new Redis(redisUri);
+
+const initRedis = async () => {
+  try {
+    const ping = await redisMaster.ping();
+    console.log(ping);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export { redisMaster, redisPub, redisSub, initRedis };
